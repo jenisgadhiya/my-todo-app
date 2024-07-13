7989feeec2a022eb53e762894a17dc875f9a96c6
Jenis Gadhiya <jenisgadhiya@Jeniss-MacBook-Air.local>
2024-07-13 12:13:51 +0530

    Integrated api

---
+++ src/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+
+instance.interceptors.response.use(
+  response => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    return response;
+  },
+  error => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
